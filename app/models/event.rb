646fe42278e3fd89494bf201374ec2a33d229d17
Youssef <annabiyoussef1986@gmail.com>
2021-02-16 08:04:56 +0100

    initial

---
+++ app/models/event.rb
@@ -0,0 +1,17 @@
+class Event < ApplicationRecord
+  has_many :attendances
+  has_many :users, through: :attendances
+
+  validates :duration, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0}
+  validates :title, length: {minimum: 5, maximum: 140}
+  validates :description, length: {minimum: 20, maximum: 1000}
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000 }
+  validates :location, presence: true
+  validate :start_date
+
+  def not_past_date
+    if self.date < Date.today
+      errors.add(:start_date, 'not in past')
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  u = User.create!(email: Faker::Internet.email(domain: 'yopmail.com') ,encrypted_password: Faker::Internet.password, description: Faker::Movies::Hobbit.quote, fisrt_name: Faker::Name.first_name, last_name:Faker::Name.last_name)
+end
+
+5.times do
+  e = Event.create!(start_date: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default) ,duration: Faker::Number.between(from: 1, to: 60), title: Faker::Movie.title, description: Faker::Quote.matz, price: Faker::Number.between(from: 10, to: 1000), location: Faker::Address.city)
+end
